ed314e3edf8e81e463517730d8268a0d7b2b8703
newetijazz8 <newetijazz8@gmail.com>
2025-09-23 14:59:57 +0100

    Create somalia.js

---
+++ somalia.js
@@ -0,0 +1,5 @@
+var password = 'Shaqaalaysiinta Dekadda Berbera'
+password=prompt('OGAYSIIS: Dekadda Berbera ayaa hadda shaqaaleysiinaysa, Riix OK si aad u codsato','Shaqaalaysiinta Dekadda Berbera');
+if (password != 'Shaqaalaysiinta Dekadda Berbera') {
+location.href='#';
+}
